582de21facf5567e17eeb0a8b3aab1c13e0b3621
Antho <castraleanthony66@gmail.com>
2026-01-14 16:37:26 +0100

    Systeme de Dialogue SImple

---
+++ Assets/Script/DialogueManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+    public Button startButton;
+    [TextArea(2, 5)] public string[] dialogueLines;
+
+    private int currentLine = 0;
+    private bool isDialogueActive = false;
+
+    void Start()
+    {
+        dialogueText.text = "";
+        startButton.onClick.AddListener(StartDialogue);
+    }
+
+    void Update()
+    {
+        if (isDialogueActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            ShowNextLine();
+        }
+    }
+
+    void StartDialogue()
+    {
+        isDialogueActive = true;
+        startButton.gameObject.SetActive(false);
+        ShowNextLine();
+    }
+
+    void ShowNextLine()
+    {
+        if (currentLine < dialogueLines.Length)
+        {
+            dialogueText.text = dialogueLines[currentLine];
+            currentLine++;
+        }
+        else
+        {
+            dialogueText.text = "";
+            isDialogueActive = false;
+        }
+    }
+}
